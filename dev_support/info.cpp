d143f2b599bb0da4889dbf0d33ef2ee2bb8ec90a
ericxu233 <xuhanyang5@gmail.com>
2021-06-06 20:50:12 -0400

    created files for device info

---
+++ dev_support/info.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <CL/sycl.hpp>
+
+
+namespace sycl = cl::sycl;
+
+int main() {
+    sycl::device device = sycl::default_selector{}.select_device();
+
+    auto work_group_size = device.get_info<sycl::info::device::max_work_group_size>();
+    std::cout << "This is max work group size: " << work_group_size << std::endl;
+
+    auto max_work_item = device.get_info<sycl::info::device::max_work_group_size>();
+    std::cout << "First dimension: " << max_work_item.get(1) << " Second dimension: " << max_work_item.get(2) << " Third dimesnion: " << max_work_item.get(3) << std::endl;
+    //continue and test it on research cluster
+}
