5fc4fd3d550c389725d228065da76cc0567068c1
ericxu233 <xuhanyang5@gmail.com>
2021-06-06 21:05:55 -0400

    correction

--- dev_support/info.cpp
+++ dev_support/info.cpp
@@ -10,7 +10,7 @@ int main() {
     auto work_group_size = device.get_info<sycl::info::device::max_work_group_size>();
     std::cout << "This is max work group size: " << work_group_size << std::endl;
 
-    auto max_work_item = device.get_info<sycl::info::device::max_work_group_size>();
+    auto max_work_item = device.get_info<sycl::info::device::max_work_item_sizes>();
     std::cout << "First dimension: " << max_work_item[1] << " Second dimension: " << max_work_item[2] << " Third dimesnion: " << max_work_item[3] << std::endl;
     //continue and test it on research cluster
 }
