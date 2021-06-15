cfd04cd79279218b67c104248e3780700bc984a1
ericxu233 <xuhanyang5@gmail.com>
2021-06-14 22:54:10 -0400

    met with supervisor on details of parallization issues

--- src/include/syclfft.h
+++ src/include/syclfft.h
@@ -12,3 +12,7 @@ void sycl_fft_setup();
 //rs means result split, essentailly meaning that the output will be split to real and imaginary parts
 void rs_parrallel(vector<float>& data, vector<float>& real, vector<float>& complex); 
 void rs_sequencial(vector<float>& data, vector<float>& real, vector<float>& complex);
+
+
+//
+void fft_optimized_parallel(vector<float>& data, vector<float>& real, vector<float>& complex);
