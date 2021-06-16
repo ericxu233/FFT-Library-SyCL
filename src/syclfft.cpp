a9643e0cae8a7724f4f3221e106dc6ce318fb8cd
ericxu233 <xuhanyang5@gmail.com>
2021-06-15 21:02:37 -0400

    moved main to syclfft.cpp for testing

--- src/syclfft.cpp
+++ src/syclfft.cpp
@@ -144,3 +144,21 @@ void rs_parrallel(vector<float>& data, vector<float>& real, vector<float>& compl
         });
     }
 }
+
+
+int main() {
+    vector<float> data = {1 , 2, 1, 3, 1, 2, 1, 2};
+    vector<float> output_r;
+    vector<float> output_c;
+
+    sycl_fft_setup();
+
+    rs_parrallel(data, output_r, output_c);
+
+    for (size_t i = 0; i < output_c.size(); i++) {
+        cout << "( " << output_r[i] << " , " << output_c[i] << " )" << endl; 
+    }
+
+
+    return 0;
+}
