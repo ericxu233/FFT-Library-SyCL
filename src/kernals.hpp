3a4400154c8739654cd1a2d891002d7df10f36f9
ericxu233 <xuhanyang5@gmail.com>
2021-07-02 22:26:21 -0400

    testing

--- src/kernals.hpp
+++ src/kernals.hpp
@@ -38,7 +38,7 @@ public:
         local_imag[index1] = 0;
         local_real[index1] = data[reverse_index];
 
-        //out << local_real[index1] << " with ind: " << index1 << sycl::endl;
+        out << local_real[index1] << " with ind: " << index1 << sycl::endl;
 
         //synchronize
         item.barrier(sycl::access::fence_space::local_space);
